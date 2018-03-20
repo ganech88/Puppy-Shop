ddccb51805fbc1e82138abf1de0a1ab60f16c04d
HMVanesa <Hna.Vanesa@smdei.com>
2018-03-20 19:24:16 -0300

    merge

---
+++ includes/footer.php
@@ -0,0 +1,40 @@
+
+<footer class="footer text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 mb-5 mb-lg-0">
+          <h4 class="text-uppercase mb-4">Atención al cliente</h4>
+          <p class="lead mb-0">Línea gratuita:
+          <br> 4809-8888 / 0810-333-5456</p>
+      </div>
+
+      <div class="col-md-4">
+          <h4 class="text-uppercase mb-4">Seguinos</h4>
+          <div class="img">
+            <img src="images/fac.png" class="img-fluid" alt="">
+          </div>
+          <div class="img">
+            <img src="images/Tw.png" class="img-fluid" alt="">
+          </div>
+          <div class="img">
+            <img src="images/instag.png" class="img-fluid" alt="">
+          </div>
+
+      </div>
+      <div class="col-md-4 mb-5 mb-lg-0">
+          <h4 class="text-uppercase mb-4">Suscribíte a nuestro newsletter</h4>
+          <label> Dejanos tu correo electrónico para recibir todas las ofertas y promociones antes que nadie
+          </label>
+          <div class="actions">
+              <input type="text" name="email" placeholder="" title="Ingresá tu correo" class="input-text required-entry validate-email">
+              <button type="submit" class="button" onclick="xt_click(this,'C','14','footer::newsletter::subscribe::ok','N')">
+              </button>
+          </div>
+      </div>
+      <div class="container final">
+          <p class="m-0 text-center text-red">Copyright © Unsere Website 2018</p>
+      </div>
+    </div>
+  </div>
+
+</footer>
