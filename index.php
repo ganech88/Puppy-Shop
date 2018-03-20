355eabca34a88a8ae6a29fd24e16ea26aa6c1751
sarichiba <sary_sc@hotmail.com>
2018-03-20 19:14:02 -0300

    Agrego imagenes, preguntas y estilos

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
       <link rel="stylesheet" href="css/bootstrap.min.css">
       <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,136 +8,105 @@
       <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
       <link rel="stylesheet" href="css/styles.css">
     <title>Home</title>
-  </head>
-  <body>
-<header>
-<?php
-require_once("includes/header-nav.php");
-?>
-</header>
+</head>
 
+<body>
+  <header class="header" >
+    <?php
+      require_once("includes/header-nav.php");
+    ?>
+  </header>
 
+<!-- Carousel -->
+<div class="row">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner" role="listbox">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="images/banner.jpg" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="images/banner2.jpg" alt="Second slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
 
-
+<!-- Sucursales -->
 <div class="row d-flex flex-wrap">
-  <div class="card col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+
+  <div class="card col-xs-12 col-sm-4 col-md-4 col-lg-6 col-xl-6">
   <div class="card-body">
     <h4 class="card-title">Microcentro</h4>
-    <img src="images/cabin.png" class="img-fluid" alt="">
+    <img src="images/sucursal1.jpg" class="img-fluid" alt="">
     <p class="card-text">
       <ul>
           <li>Direccion</li>
           <li>Telefono</li>
           <li><a href="#!" class="card-link alling">Como llegar?</a></li>
       </ul>
-
     </p>
-
   </div>
 </div>
 
-<div class="card col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-<div class="card-body">
-  <h4 class="card-title">Belgrano</h4>
-  <img src="images/cabin.png" class="img-fluid" alt="">
-  <p class="card-text">
-    <ul>
+  <div class="card col-xs-12 col-sm-4 col-md-4 col-lg-6 col-xl-6">
+  <div class="card-body">
+    <h4 class="card-title">Belgrano</h4>
+    <img src="images/sucursal5.jpg" class="img-fluid" alt="">
+    <p class="card-text">
+      <ul>
         <li>Direccion</li>
         <li>Telefono</li>
-
-
-    </ul>
-  </p>
-  <a href="#!" class="card-link">Como llegar?</a>
-</div>
+        <li><a href="#!" class="card-link">Como llegar?</a></li>
+      </ul>
+    </p>
+  </div>
 </div>
 
-<div class="card col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-<div class="card-body">
-  <h4 class="card-title">Recoleta</h4>
-  <img src="images/cabin.png" class="img-fluid" alt="">
-  <p class="card-text">
-    <ul>
+  <div class="card col-xs-12 col-sm-4 col-md-4 col-lg-6 col-xl-6">
+  <div class="card-body">
+    <h4 class="card-title">Recoleta</h4>
+    <img src="images/sucursal3.jpg" class="img-fluid" alt="">
+    <p class="card-text">
+      <ul>
         <li>Direccion</li>
         <li>Telefono</li>
-
-
-    </ul>
-  </p>
-  <a href="#!" class="card-link">Como llegar?</a>
-</div>
-</div>
-
-
-  <div class="card col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-    <div class="card-body">
-  <h4 class="card-title">Palermo</h4>
-  <img src="images/cabin.png" class="img-fluid" alt="">
-  <p class="card-text">
-    <ul>
-        <li>Direccion</li>
-        <li>Telefono</li>
-
-
-    </ul>
-  </p>
-  <a href="#!" class="card-link">Como llegar?</a>
-
-    </div>
+        <li><a href="#!" class="card-link">Como llegar?</a></li>
+      </ul>
+    </p>
+  </div>
   </div>
 
-
+  <div class="card col-xs-12 col-sm-4 col-md-4 col-lg-6 col-xl-6">
+  <div class="card-body">
+    <h4 class="card-title">Palermo</h4>
+    <img src="images/sucursal4.jpg" class="img-fluid" alt="">
+    <p class="card-text">
+      <ul>
+        <li>Direccion</li>
+        <li>Telefono</li>
+        <li><a href="#!" class="card-link">Como llegar?</a></li>
+      </ul>
+    </p>
+  </div>
+  </div>
 
 </div>
 
-<footer class="footer text-center">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4 mb-5 mb-lg-0">
-          <h4 class="text-uppercase mb-4">Atención al cliente</h4>
-          <p class="lead mb-0">Línea gratuita:
-          <br> 4809-8888 / 0810-333-5456</p>
-      </div>
+<footer>
 
-      <div class="col-md-4">
-          <h4 class="text-uppercase mb-4">Seguinos</h4>
-          <div class="img">
-            <img src="images/facebook.png" class="img-fluid" alt="">
-          </div>
-          <div class="img">
-            <img src="images/twitter.png" class="img-fluid" alt="">
-          </div>
-          <div class="img">
-            <img src="images/Instagram.png" class="img-fluid" alt="">
-          </div>
-
-          <!-- <p class="lead mb-0">Freelance is free and open source landing page theme created to
-          <a href="http://startbootstrap.com">your free personal use</a>.</p> -->
-      </div>
-      <div class="col-md-4 mb-5 mb-lg-0">
-          <h4 class="text-uppercase mb-4">Suscribíte a nuestro newsletter</h4>
-          <label> Dejanos tu correo electrónico para recibir todas las ofertas y promociones antes que nadie
-          </label>
-          <div class="actions">
-              <input type="text" name="email" placeholder="Ingresá tu correo" title="Ingresá tu correo" class="input-text required-entry validate-email">
-              <button type="submit" class="button" onclick="xt_click(this,'C','14','footer::newsletter::subscribe::ok','N')">
-              </button>
-          </div>
-      </div>
-      <div class="container">
-          <p class="m-0 text-center text-red">Copyright © Unsere Website 2018</p>
-      </div>
-    </div>
-  </div>
 
 </footer>
-
-
     <!-- Jquery -->
     <script src="js/jquery-3.3.1.min.js"></script>
     <!--bootstrap  -->
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
-
-  <!-- <button type="submit" class="button" onclick="xt_click(this,'C','14','footer::newsletter::subscribe::ok','N')"> -->
--- preguntas.php
+++ preguntas.php
@@ -15,32 +15,108 @@
 require_once("includes/header-nav.php");
    ?>
 </header>
+<br><br>
 
-<div class="row">
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner" role="listbox">
-      <div class="carousel-item active">
-        <img class="d-block w-100" src="#" data-src="holder.js/900x400?theme=social" alt="First slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="#" data-src="holder.js/900x400?theme=industrial" alt="Second slide">
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>  
+<div class="contenedor">
+  <br>
 
+  <div class="titulo success">
+    <h3> Preguntas Frecuentes </h3>
+  </div>
+
+<!-- <div class="container"> -->
+	<div class="row">
+	    <ul id="accordion" class="col-sm-6 col-md-12">
+
+  <!-- Question one -->
+	      <li>
+	       <div id="choose" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" >
+	        Necesito estar registrado para realizar una compra?
+	        <span class="fa fa-chevron-up fa-1x text-info pull-right"></span>
+	       </div>
+
+         <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+          <div class="card-body">
+           No, no es necesario crear una cuenta para poder comprar en Puppyshop. Pero tené en cuenta que al hacerlo podrás llevar un control de las compras y movimientos realizados en nuestra tienda online.
+          </div>
+         </div>
+	      </li>
+
+	 <!-- Question two -->
+	      <li>
+	       <div class="collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+	        Cómo se puede pagar?
+	        <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
+	       </div>
+
+         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+           <div class="card-body">
+            Podes abonar los productos con tarjeta de crédito y débito, depósito bancario, y en efectivo si los pasas a buscar por nuestros locales.
+            En cualquier caso esperaremos la acreditación del pago para enviarte el producto.
+           </div>
+         </div>
+	      </li>
+
+	  <!-- Question three -->
+	      <li>
+	       <div class="collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+	        Se realizan entregas a domicilio? Y al interior del país?
+	        <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
+	       </div>
+
+         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+          <div class="card-body">
+           Realizamos envíos a todo el país. Si tu compra es superior a 2 mil pesos, el envío es sin costo.
+          </div>
+         </div>
+	      </li>
+
+	   <!-- Question Four -->
+	        <li>
+	         <div class="collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+	          Cuánto tiempo demora la entrega?
+	          <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
+	         </div>
+
+           <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+            <div class="card-body">
+             sciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+           </div>
+	        </li>
+
+	   <!-- Questiion Five -->
+	        <li>
+	         <div class="collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+	          How can I join the team?
+	          <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
+	         </div>
+
+           <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+            <div class="card-body">
+             Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+           </div>
+	        </li>
+
+	   <!-- Question Six-->
+	        <li>
+	         <div class="collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+	          In case I forgot my password, what do I do?
+	          <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
+	         </div>
+
+           <div id="collapseSix" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body">
+             Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+           </div>
+	        </li>
+
+	    </ul>
+	</div>
 </div>
-
-
-
-
+</div>
 
 <footer>
 
@@ -50,5 +126,6 @@ require_once("includes/header-nav.php");
     <script src="js/jquery-3.3.1.min.js"></script>
     <!--bootstrap  -->
     <script src="js/bootstrap.min.js"></script>
-  </body>
+
+</body>
 </html>
